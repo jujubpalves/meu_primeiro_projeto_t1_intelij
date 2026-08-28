cad3a93d9ad8887fde7d8482fc3b91a0b7884e99
JULIA BEATRIZ PEREIRA ALVES <julia.alves68@senaisp.edu.br>
2026-08-28 14:50:36 -0300

    feat: Add new function

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+    public static void main(String[] args) {
+        int valorInt = 12;
+        double valorDouble = valorInt;
+        System.out.println("Valor do Double: " + valorDouble);
+    }
+}
+
