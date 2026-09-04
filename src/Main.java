221f7d7ad1f59ccccd92b9424c84dd89d76cc17c
56576890893 <juju.bpalves@gmail.com>
2026-09-04 13:52:31 -0300

    Exercício 6

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,16 @@
-package org.example;
 import java.util.Scanner;
 
-// Exercício 4
+// Exercício 6 - Direito ao Vale Refeição
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double nota = sc.nextDouble();
+        double salario = sc.nextDouble();
 
 
-        if (nota >= 9 ){
-            System.out.println("Excelente");
-        } else if(nota <= 7) {
-            System.out.println("Bom");
-        }
-
-        if (nota >= 5 ){
-            System.out.println("Regular");
-        } else  {
-            System.out.println("Insuficiente");
+        if (salario <= 4000) {
+            System.out.println("Você possui direito ao VR.");
+        } else {
+            System.out.println("Você não possui direito ao VR.");
         }
     }
+}
