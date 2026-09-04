7c4a5abcf20af55c38eca92e99fb05b19d04092a
56576890893 <juju.bpalves@gmail.com>
2026-09-04 14:17:59 -0300

    Exercício 8

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
-// Exercício 7 - Plano de Saúde Empresarial
+// Exercício 8 - Auxílio Combustível
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite o tempo de contratação (meses): ");
-        double tempo = sc.nextDouble();
+        System.out.print("Você possui veículo próprio? (sim/nao): ");
+         String resposta  = sc.nextLine();
 
 
-        if (tempo >= 3) {
-            System.out.println("O plano de saúde  está disponível para você.");
+        if (resposta.equalsIgnoreCase("Sim")) {
+            System.out.println("Auxílio combustível disponível.");
         } else {
-            System.out.println("Serviço não disponível para você.");
+            System.out.println("Não possui auxílio combustível.");
         }
     }
 }
