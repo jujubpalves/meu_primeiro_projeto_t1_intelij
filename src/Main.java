244be64afa91c0acd343df340091ab6ab74b8a04
56576890893 <juju.bpalves@gmail.com>
2026-09-09 16:10:52 -0300

    Nivel 3- Exercício 1

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,16 @@
 import java.util.Scanner;
 
-// Nivel 2 - Exercicio 2
+//Nivel 3 - Exercicio 1
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-
-        System.out.print("Digite a sua nota de 0 a 10: ");
-        double nota  = sc.nextDouble();
-
-        boolean cond1 = ((nota >= 7.5) && (nota <= 10));
-        boolean cond2 = ((nota >= 6) && (nota <= 7.5));
-        boolean cond3 = ((nota >= 5) && (nota <= 6));
-
-
-        if ( cond1 ) {
-            System.out.println("Nota Excelente.");
-        }else if (cond2){
-            System.out.println("Nota Boa.");
-        }else if (cond3){
-        System.out.println("Nota Regular.");
-        }else if (nota>10){
-            System.out.println("Valor incorreto.");
-        }else {
-        System.out.println("Reprovado.");
+        System.out.println("por favor, diga sua nota de ingles (10 a 0)");
+        double nota1= sc.nextDouble ();
+        System.out.println("por favor, diga sua nota de matemática (10 a 0)");
+        double nota2= sc.nextDouble ();
+        System.out.println("por favor, diga sua nota de portugues (10 a 0)");
+        double nota3= sc.nextDouble ();
+        double media = (nota1 + nota2 + nota3)/ 3;
+        System.out.println ("média:\n" + media);
     }
-    }
-    }
+}
