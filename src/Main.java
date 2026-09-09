c744447e0b01aacd06b0826f126bf9a131ff53cd
56576890893 <juju.bpalves@gmail.com>
2026-09-09 16:15:39 -0300

    Nivel 4- Exercício 2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
-//Nivel 4 - Exercicio
+//Nivel 4 - Exercicio 2
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("digite o valor do real:");
-        double real= sc.nextDouble ();
+        System.out.print("diga quantos kilometros o carro consegue percorrer com o tanque cheio: ");
+        int n1 = sc.nextInt();
+        System.out.print("agora diga quantos litros tem o tanque: ");
+        int n2 = sc.nextInt();
+        int n3 = n1 / n2;
+        System.out.println("Seu carro faz " + n3 + " Km/l de combustivel");
 
-        double dolar = real*0.1941;
-
-
-        System.out.println ("valor do real convertido em dolar:\n" + "U$" +
-                dolar );
     }
 }
-
 
