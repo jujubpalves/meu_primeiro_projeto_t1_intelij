26ab4b869e4c349f86855cf529cb33c72d116ffa
56576890893 <juju.bpalves@gmail.com>
2026-09-09 16:16:27 -0300

    Nivel 4- Exercício 3

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
-//Nivel 4 - Exercicio 2
+//Nivel 4 - Exercicio 3
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("diga quantos kilometros o carro consegue percorrer com o tanque cheio: ");
-        int n1 = sc.nextInt();
-        System.out.print("agora diga quantos litros tem o tanque: ");
-        int n2 = sc.nextInt();
-        int n3 = n1 / n2;
-        System.out.println("Seu carro faz " + n3 + " Km/l de combustivel");
-
+        Scanner sc = new Scanner (System.in);
+        System.out.print("digite o valor da hora trabalhada: ");
+        int n1 = sc.nextInt ();
+        System.out.print("agora diga quantas horas foram trabalhadas nesse mês: ");
+        int n2 = sc.nextInt ();
+        int n3 = n1*n2;
+        System.out.println("Seu salário este mês é de R$ " + n3 + ",00");
     }
 }
-
