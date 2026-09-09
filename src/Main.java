913a8448c7ccaab30cdcd916b20669fad1d46faf
56576890893 <juju.bpalves@gmail.com>
2026-09-09 16:17:05 -0300

    Nivel 4- Exercício 4

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-//Nivel 4 - Exercicio 3
+//Nivel 4 - Exercicio 4
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.print("digite o valor da hora trabalhada: ");
-        int n1 = sc.nextInt ();
-        System.out.print("agora diga quantas horas foram trabalhadas nesse mês: ");
-        int n2 = sc.nextInt ();
-        int n3 = n1*n2;
-        System.out.println("Seu salário este mês é de R$ " + n3 + ",00");
+        System.out.print("digite o valor do raio [cm]: ");
+        double n1 = sc.nextDouble ();
+
+        double pi = 3.14;
+        double A = pi*(n1*n1);
+        System.out.println("\nA área do círculo é de " + A + "cm²");
+        System.out.println("\nEm metros, " + A/10000 + "m²\n");
     }
 }
