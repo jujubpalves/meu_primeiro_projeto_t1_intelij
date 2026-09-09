988ee9c87add378a57ad2f144e908fa31e45e386
56576890893 <juju.bpalves@gmail.com>
2026-09-09 16:19:52 -0300

    Nivel 5- Exercício 3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
-//Nivel 5 - Exercicio 2
+//Nivel 5 - Exercicio 3
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        System.out.print("digite seu peso [kg]: ");
+        int n1 = sc.nextInt ();
 
         System.out.print("digite a altura [m]: ");
-        double alt = sc.nextDouble ();
-        System.out.print("altura em metros: " + alt + "\naltura em centimetros: " + (alt * 100) + "\naltura em milimetros: " + (alt * 1000));
+        int n2 = sc.nextInt();
+        int n3 = n1/n2;
+        System.out.println("Resultado: " + n3 + "\n resto da divisão: " +
+                n1%n2);
     }
 }
