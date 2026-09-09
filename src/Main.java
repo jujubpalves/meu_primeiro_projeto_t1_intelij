93bdbb6863bbd5d3b3ad45ff45f1283af3a4b182
56576890893 <juju.bpalves@gmail.com>
2026-09-09 16:19:23 -0300

    Nivel 5- Exercício 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
 
-//Nivel 5 - Exercicio 1
+//Nivel 5 - Exercicio 2
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.print("digite seu peso [kg]: ");
-        double peso = sc.nextDouble ();
 
-        System.out.print("digite sua altura [m]: ");
+        System.out.print("digite a altura [m]: ");
         double alt = sc.nextDouble ();
-        double IMC = peso / (alt * alt);
-        System.out.println("Seu IMC é: " + IMC);
+        System.out.print("altura em metros: " + alt + "\naltura em centimetros: " + (alt * 100) + "\naltura em milimetros: " + (alt * 1000));
     }
 }
