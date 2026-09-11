d1bca00150bc87c75341faae12fba1cd7bb8c779
56576890893 <juju.bpalves@gmail.com>
2026-09-11 13:42:42 -0300

    Exercício 13 - E Detecção de Transação Suspeita

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,31 @@
 package org.example;
+
 import java.util.Scanner;
 
 // Exercicios de estrutura de decisão
-// Exercício 12 - Elegibilidade para Cartão Premium
+
+// Exercício 13 - E Detecção de Transação Suspeita
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite a renda mensal do cliente (R$):");
-        double renda = entrada.nextDouble();
 
-        System.out.println("Digite o score de crédito do cliente:");
-        int score = entrada.nextInt();
+        System.out.println("Digite o valor da transação (R$):");
 
-        if (renda > 8000 && score > 700) {
-            System.out.println("Cartão Premium aprovado!");
+        double valorTransacao = entrada.nextDouble();
+
+        if (valorTransacao > 10000) {
+
+            System.out.println("Alerta: Transação suspeita! Sinalizada para análise.");
+
         } else {
-            System.out.println("Cartão Premium negado. É necessário ter renda acima de R$ 8.000 e score acima de 700.");
+
+            System.out.println("Transação normal aprovada.");
+
         }
+
     }
+
 }
