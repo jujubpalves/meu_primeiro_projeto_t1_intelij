baa373cf885468eed2684ec4f47b24c19ce077c1
56576890893 <juju.bpalves@gmail.com>
2026-09-11 14:14:51 -0300

    Atividade 2 - Controle de Produção Industrial

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,9 @@
-package org.example;
-
-import java.util.Scanner;
-
-// Exercicios de estrutura de decisão
-
-// Exercício 14 - Concessão de Isenção de Tarifas
+// Atividade 2 - Controle de Produção Industrial
 
 public class Main {
-
     public static void main(String[] args) {
-
-        Scanner entrada = new Scanner(System.in);
-
-        System.out.println("Digite o saldo médio mensal do cliente (R$):");
-
-        double saldo = entrada.nextDouble();
-
-        if (saldo > 5000) {
-
-            System.out.println("Direito à isenção de tarifa bancária concedido.");
-
-        } else {
-
-            System.out.println("Tarifa bancária mantida. É necessário saldo superior a R$ 5.000 para isenção.");
-
+        for (int peca = 1; peca <= 20; peca++) {
+            System.out.println("Peça Produzida");
         }
-
     }
-
 }
