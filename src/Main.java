d402c84e7a55a59b075d46147817919e8638a227
56576890893 <juju.bpalves@gmail.com>
2026-09-14 13:39:00 -0300

    Exercício 3 - Soma de Vendas do Diaa

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
-// Atividade 2 - Controle de Produção Industrial
+//Estrtura de repetição
+// Exercício 3 - Soma de Vendas do Diaa
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (int peca = 1; peca <= 20; peca++) {
-            System.out.println("Peça Produzida");
+        Scanner entrada = new Scanner(System.in);
+        double total = 0;
+        for (int venda = 1; venda <= 5; venda++) {
+            System.out.println("Digite o valor da venda " + venda);
+            double valor = entrada.nextDouble();
+            total += valor;
         }
+        System.out.println("Faturamento total do dia: R$ " + total);
     }
 }
