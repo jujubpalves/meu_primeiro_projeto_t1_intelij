de726eacfe97ee8a60ffb8f63c055280efddadee
56576890893 <juju.bpalves@gmail.com>
2026-09-14 14:26:40 -0300

    Atividade 4 - Pesquisa de Satisfação

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
-//Estrtura de repetição
-// Exercício 3 - Soma de Vendas do Diaa
 import java.util.Scanner;
 
+//Estrtura de repetição
+// Atividade 4 - Pesquisa de Satisfação
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double total = 0;
-        for (int venda = 1; venda <= 5; venda++) {
-            System.out.println("Digite o valor da venda " + venda);
-            double valor = entrada.nextDouble();
-            total += valor;
+        int notaSatisfacao = 0;
+        int qtdCliente = 0;
+
+        System.out.println("Informe a sua nota: ");
+
+        while (qtdCliente < 10) {
+            qtdCliente++;
+            System.out.println("Cliente " +  qtdCliente + ", informe a sua nota: ");
+            notaSatisfacao = entrada.nextInt();
+
+
         }
-        System.out.println("Faturamento total do dia: R$ " + total);
+        System.out.println("Quantidade de clientes participantes " + qtdCliente);
     }
 }
